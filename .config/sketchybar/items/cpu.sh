69dc9b8acc80992c36e216a6719b637eab0c4906
DatNguyen <dat.n@sharering.network>
2023-02-20 15:53:40 +0700

    remove spacebar use sketchybar

---
+++ .config/sketchybar/items/cpu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source "$HOME/.config/sketchybar/colors.sh"
+
+cpu_percent=(
+	label.font="$FONT:Heavy:12"
+	label=CPU%
+	label.color="$TEXT"
+	icon="$CPU"
+	icon.color="$BLUE"
+	update_freq=2
+	mach_helper="$HELPER"
+)
+
+sketchybar 	--add item cpu.percent right 					\
+						--set cpu.percent "${cpu_percent[@]}"
