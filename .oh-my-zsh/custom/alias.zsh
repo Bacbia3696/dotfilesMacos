1c592b0d6a75409b46febc8cf46dba83d797ab93
DatNguyen <bacbia3696@gmail.com>
2023-05-28 16:12:55 +0700

    feat: update

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -8,12 +8,13 @@ alias k=kubectl
 alias i=ignite
 alias c=cargo
 alias g=git
-alias e=nvim
+alias e="OPENAI_API_KEY=$(gpg --decrypt ~/.config/nvim/chatgpt.gpg 2>/dev/null) nvim"
 alias cl=clear
 alias p=pnpm
 alias px=pnpx
 alias ef="nvim -c 'Telescope find_files'"
 alias bathelp="bat -p -l help"
+alias ssh='TERM=xterm-256color ssh'
 
 alias sdcv="sdcv -2 ~/Library/stardict/dic"
 alias gd='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
